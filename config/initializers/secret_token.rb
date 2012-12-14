13fe8a15366e58fa053e280b3ff982ca9cb3d1ca
Sri Prasanna <k.sriprasanna@gmail.com>
2012-12-15 00:07:47 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToiletWatcher::Application.config.secret_token = '8cba8645ee0edf87383ad07b42ce01800a0026a04d5dae892214505904dbc5e5253c47f97937be800b5bf20b369fffec77471c75a6c71b9e400763cb83d80ea4'
