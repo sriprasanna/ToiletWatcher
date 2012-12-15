9c3af0a0c023737baaec14cb43b656252419f0a1
Subhas Dandapani <subhasda@thoughtworks.com>
2012-12-16 02:22:08 +0300

    Dashboard styling for graphs in show page

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -6,7 +6,7 @@ class Stat < ActiveRecord::Base
   scope :today, lambda { where('time >= ? and time < ?', Date.today.to_datetime, Date.tomorrow.to_datetime) }
 
   def wash_level
-    (wash_basin_count.to_f / toilet_count.to_f) rescue 0
+    (wash_basin_count.to_f / toilet_count.to_f * 100.0) rescue 0
   end
 
   def alert_water_level?
@@ -14,6 +14,6 @@ class Stat < ActiveRecord::Base
   end
 
   def alert_wash_level?
-    wash_level > 0 and wash_level < 0.5 rescue false
+    wash_level > 0 and wash_level < 50 rescue false
   end
 end
