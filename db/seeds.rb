fe6e1179e9501d5c70bfb40835ceaa3b6b19b7be
Subhas Dandapani <subhasda@thoughtworks.com>
2012-12-15 17:21:03 +0300

    Aesthetic changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 def populate_stats(location)
-  toilet_count = 0
-  wash_basin_count = 0
-  water_level = 100
   time = Date.today.to_datetime
+  water_level = 1000
 
   toilet_seed = (3..8).to_a
   wash_basin_seed = (2..5).to_a
   time_seed = (30..60).to_a
+  water_level_seed = (10..15).to_a
 
   (1..100).each do
     time = time + time_seed.sample.minutes
+    water_level = water_level - water_level_seed
     Stat.create location: location, time: time, toilet_count: toilet_seed.sample, wash_basin_count: wash_basin_seed.sample, water_level: water_level
   end
 end
